c0e061a0397d0e1f5af22c445a2bb0b5128e69bf
Ahuriire Violah <kimvioraahuriire@gmail.com>
2025-03-23 10:18:23 +0300

    created project

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+
+# Create your views here.
+def homepage(request):
+    return HttpResponse("Hi World,  This is Viola")
+
+def contact(request):
+    return HttpResponse("Contact us on the following numbers 0707419671 ")
+
+from django.shortcuts import render
+def login(request):
+    return render(request,'login.html')
